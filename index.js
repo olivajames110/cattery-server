3d9008a2be01468ac9aa28c94c61b25709feb23e
olivajames110 <olivajames110@gmail.com>
2020-02-12 10:04:43 -0500

    updated to pull server state

--- index.js
+++ index.js
@@ -2,40 +2,28 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 'https://cattery-server.herokuapp.com/';
+const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 'https://cattery-server.herokuapp.com/';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 console.log(process.env.PORT);
 
-let SERVER_PARTY = [
-	{
-		description           : null,
-		reservationTime       : null,
-		id                    : 0.9322715463474562,
-		isReservation         : false,
-		isOverdue             : false,
-		isUpcomingReservation : false,
-		isOverlap             : false,
-		name                  : null,
-		numberInParty         : 4,
-		paid                  : true,
-		rowNum                : 1,
-		times                 : { minute: 34, hour: 19, start: '7:34 PM', end: '8:34 PM', timeStamp: 1581035640 }
-	}
-];
+let SERVER_STATE = [];
 
 io.on('connection', (socket) => {
 	console.log('New Connection');
 
 	socket.on('client_send', (socketState) => {
-		// console.log(clientState);
-		// console.log([...socketState, ...SERVER_PARTY]);
+		SERVER_STATE = [ ...socketState ];
+		socket.broadcast.emit('server_send', SERVER_STATE);
+		socket.emit('server_send', SERVER_STATE);
+	});
 
-		socket.broadcast.emit('server_send', [ ...socketState ]);
-		socket.emit('server_send', [ ...socketState ]);
+	socket.on('refresh_data', () => {
+		socket.broadcast.emit('server_send', SERVER_STATE);
+		socket.emit('server_send', SERVER_STATE);
 	});
 
 	socket.on('disconnect', () => {
